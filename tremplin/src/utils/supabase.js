7a5e814dbb2c87171b813b386dc7a0b4c36d0735
abir <abiralaoui2582@gmail.com>
2023-08-17 14:39:15 +0100

    Supprimer tout le contenu de la branche

--- tremplin/src/utils/supabase.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseURL = "https://ehdqwtwtyofhjtxmqizo.supabase.co"
-const supabaseAnonPublic = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoZHF3dHd0eW9maGp0eG1xaXpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM2MjQ4MTUsImV4cCI6MTk5OTIwMDgxNX0.6XvUTIAc7npJwp66gvEtrJNb6KenwnCA2dWTbVh8rkc"
-export default createClient(supabaseURL, supabaseAnonPublic);
